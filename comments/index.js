491de79dce2785a9ec8d6ca4f8d38000cdacd568
André Pereira <andrelcop@gmail.com>
2023-02-18 16:24:47 +0100

    add the events url on post and comments service

--- comments/index.js
+++ comments/index.js
@@ -8,18 +8,27 @@ const commentsByPostId = {};
 app.use(bodyParser.json());
 app.use(cors());
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id]);
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body
     const comments = commentsByPostId[req.params.id] || []
     comments.push({ id: commentId, content })
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: { id: commentId, content, postId: req.params.id }
+    })
+
     res.status(201).send(comments)
 });
-
+app.post('/events', (req, res) => {
+    console.log('Received Event:', req.body.type);
+    res.send({});
+});
 app.listen(4001, () => {
     console.log('Post Listening on 4001');
 });
