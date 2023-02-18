628f15e34b3cf676d8581df3ae0716e956fbb43b
André Pereira <andrelcop@gmail.com>
2023-02-18 17:43:23 +0100

    corect event -bus

--- comments/index.js
+++ comments/index.js
@@ -1,34 +1,47 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser')
-const app = express();
-const cors = require('cors')
-const commentsByPostId = {};
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body
-    const comments = commentsByPostId[req.params.id] || []
-    comments.push({ id: commentId, content })
-    commentsByPostId[req.params.id] = comments;
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: { id: commentId, content, postId: req.params.id }
-    })
+  const comments = commentsByPostId[req.params.id] || [];
 
-    res.status(201).send(comments)
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
 });
-app.post('/events', (req, res) => {
-    console.log('Received Event:', req.body.type);
-    res.send({});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received", req.body.type);
+
+  res.send({});
 });
+
 app.listen(4001, () => {
-    console.log('Post Listening on 4001');
+  console.log("Listening on 4001");
 });
--- query/index.js
+++ query/index.js
@@ -3,29 +3,36 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {}
+const posts = {};
 
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
+
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
+
     if (type === 'PostCreated') {
         const { id, title } = data;
+
         posts[id] = { id, title, comments: [] };
     }
+
     if (type === 'CommentCreated') {
         const { id, content, postId } = data;
-        const post = post[postId];
-        post.comments.push({ id, content })
+
+        const post = posts[postId];
+        post.comments.push({ id, content });
     }
+
     console.log(posts);
+
     res.send({});
 });
 
 app.listen(4002, () => {
     console.log('Listening on 4002');
-})
+});
