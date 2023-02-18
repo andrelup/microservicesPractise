9f8bc0f0b581cdb6d02628ca07d3f7baa6a3cac4
André Pereira <andrelcop@gmail.com>
2023-02-19 00:56:13 +0100

    construct a handler for a missing events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,14 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
-  console.log(event)
+  console.log(event);
+
+  events.push(event);
+
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -21,6 +26,10 @@ app.post("/events", (req, res) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
+});
+
+app.get("/get", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
