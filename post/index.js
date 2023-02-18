ae49f68e902f5ef08669b5dadd3f1450788cc405
André Pereira <andrelcop@gmail.com>
2023-02-18 14:43:26 +0100

    add cors

--- post/index.js
+++ post/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser')
 const app = express();
+const cors = require('cors')
 
 const posts = {};
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
@@ -14,10 +15,10 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body
-    post[id] = {
+    posts[id] = {
         id, title
     }
-    res.status(201).send(post);
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
